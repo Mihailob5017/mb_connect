53c375b8f3d24bcd415f0f616092eda61f62dd5d
Mihailob5017 <mihailob5017@gmail.com>
2021-09-01 22:53:26 +0200

    set up the backend index file and connected the app to mongodb

---
+++ server/index.js
@@ -0,0 +1,40 @@
+// Importing Required Libraries
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+// Importing Routes
+
+// Enabling env so we can keep all our secrets there
+dotenv.config();
+
+// Setting up all the constants
+const MONGODB_URL = process.env.MONGODB_URL;
+
+const PORT = process.env.PORT;
+// Start the express server
+const app = express();
+
+// Limit the maximum size of the transfer
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+
+// Enable Cross Site Sharing
+app.use(cors());
+
+// Connecting to MongoDB
+mongoose
+	.connect(MONGODB_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(`Server running on PORT ${PORT} and is connected to MongoDB`);
+		});
+	})
+	.catch((err) => {
+		console.error(err.message);
+	});
