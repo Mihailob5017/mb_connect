7adbb5c9da09dee7372890cfca11b980a288ed0e
Mihailob5017 <mihailob5017@gmail.com>
2021-11-04 14:27:32 +0100

    sign up overhaul for experts finished

--- client/src/pages/Sign_Up/sign_up_expert.js
+++ client/src/pages/Sign_Up/sign_up_expert.js
@@ -4,8 +4,7 @@ import FileBase from 'react-file-base64';
 import Upload from '../../images/upload.svg';
 
 // Components
-import GreenScreen from '../../components/green-screen/green-screen.component';
-import CardComponent from '../../components/card/card.component';
+
 import CardHeader from '../../components/card/card-header/card-header.component';
 import InputComponent from '../../components/input/input.component';
 import Button from '../../components/button/button.component';
@@ -17,60 +16,46 @@ const SignUpComponent = ({ state, handleChange, handleSubmit, options }) => {
 		'Upload a Profile Picture'
 	);
 	return (
-		<div className='sign-up-container '>
-			<GreenScreen reduced={true} />
-			<CardComponent customClass='sign-up-card'>
-				<div className='sign-up-column'>
-					<CardHeader /> <h1 className='sign-up-head'>Create your Account</h1>
-					<InputComponent
-						value={state.email}
-						handleChange={handleChange}
-						type='email'
-						name='email'
-						label='Email Address'
-					/>
+		<div className='sign-up-wrapper'>
+			<div className='sign-up-container'>
+				<CardHeader />
+				<div className='border-div'></div>
+				<h1 className='ca-head'>Create your expert account</h1>
+				<p className='ca-text'>
+					Make sure to double check all the information you entered since you
+					won't be able to change any of it later on.
+				</p>
+				<div className='row-div'>
 					<InputComponent
 						value={state.first_name}
 						handleChange={handleChange}
 						name='first_name'
+						customClass='input-small'
 						label='First Name'
 					/>
 					<InputComponent
 						value={state.last_name}
 						handleChange={handleChange}
 						name='last_name'
+						customClass='input-small'
 						label='Last Name'
 					/>
-					<DropDownComponent
-						value={state.service}
-						handleChange={handleChange}
-						options={options}
-					/>
-					<div className='about-me-wrapper'>
-						<label>About me</label>
-						<textarea
-							value={state.about_me}
-							name='about_me'
-							onChange={(e) => handleChange(e.target.name, e.target.value)}
-							className='about-me-textarea'
-						></textarea>
-					</div>
 				</div>
-
-				<div className='sign-up-column'>
-					<div className='empty-space'></div>
-					<InputComponent
-						value={state.price}
-						handleChange={handleChange}
-						type='number'
-						name='price'
-						label='Hourly Price'
-					/>
+				<InputComponent
+					value={state.email}
+					handleChange={handleChange}
+					type='email'
+					customClass='input-small email-input'
+					name='email'
+					label='Email Address'
+				/>
+				<div className='row-div'>
 					<InputComponent
 						value={state.password}
 						handleChange={handleChange}
 						isPassword={true}
 						type='password'
+						customClass='input-small'
 						name='password'
 						label='Password'
 					/>
@@ -79,32 +64,54 @@ const SignUpComponent = ({ state, handleChange, handleSubmit, options }) => {
 						handleChange={handleChange}
 						isPassword={true}
 						type='password'
+						customClass='input-small'
 						name='repeat_password'
 						label='Repeat Password'
 					/>
-					<div className='upload-wrapper'>
-						<FileBase
-							type='file'
-							multiple={false}
-							onDone={({ base64 }) => {
-								handleChange('profile_pic', base64);
-								setUploadBtnText('Image Selected');
-							}}
-						/>
-						<img src={Upload} alt='Upload' />
-						<p>{uploadBtnText}</p>
-					</div>
-					<p className='sign-up-text'>
-						Make sure you to check if all the parameters that you entered are
-						correct, since you won’t be able to change them once the profile is
-						created
-					</p>
+				</div>{' '}
+				<div className='about-me-wrapper'>
+					<label>About me</label>
+					<textarea
+						value={state.about_me}
+						name='about_me'
+						onChange={(e) => handleChange(e.target.name, e.target.value)}
+						className='about-me-textarea'
+					></textarea>
+				</div>
+				<div className='row-div align'>
+					<DropDownComponent
+						value={state.service}
+						handleChange={handleChange}
+						options={options}
+					/>{' '}
+					<InputComponent
+						value={state.price}
+						handleChange={handleChange}
+						type='number'
+						name='price'
+						customClass='input-small hourly-input'
+						label='Hourly Price'
+					/>
+				</div>{' '}
+				<div className='upload-wrapper'>
+					<FileBase
+						type='file'
+						multiple={false}
+						onDone={({ base64 }) => {
+							handleChange('profile_pic', base64);
+							setUploadBtnText('Image Selected');
+						}}
+					/>
+					<img src={Upload} alt='Upload' />
+					<p>{uploadBtnText}</p>
+				</div>
+				<div className='bottom-element'>
 					<Button handleClick={handleSubmit} type='primary'>
 						Create Profile
 					</Button>
 					<StageCounter stage={2} />
 				</div>
-			</CardComponent>
+			</div>
 		</div>
 	);
 };
